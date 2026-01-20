eb381c518e558b66eefbe6bdcff881f3e0a021bc
fangbz <873067641@qq.com>
2026-01-20 10:56:12 +0800

    feat：添加license

--- 12-搬运公服统一能力输出接口/prod-outside-update-api-list.js
+++ 12-搬运公服统一能力输出接口/prod-outside-update-api-list.js
@@ -8,6 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=rs.com
 // @run-at       document-end
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
